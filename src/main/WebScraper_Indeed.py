a3b2bd99cafd0e7dd9d65f7d5c0a427d555fc922
MorphingGrid <duyetvan7@gmail.com>
2023-05-01 15:46:32 -0400

    Update WebScraper_Indeed.py

--- src/main/WebScraper_Indeed.py
+++ src/main/WebScraper_Indeed.py
@@ -14,18 +14,19 @@ def autobot():
     
     search_terms = ['Data Analyst', 'Data Scientist', 'Data Engineer']
     
-    driver.get('https://www.indeed.com/?from=gnav-homepage')
-    search_input = driver.find_element_by_id('text-input-what')
-    search_input.send_keys('Data Analyst')
-    search_input.send_keys(Keys.RETURN)    
-    time.sleep(3)
-    scraper(driver)
+    for word in search_terms:
+        driver.get('https://www.indeed.com/?from=gnav-homepage')
+        search_input = driver.find_element_by_id('text-input-what')
+        search_input.send_keys(word)
+        search_input.send_keys(Keys.RETURN)    
+        time.sleep(3)
+        scraper(driver, word)
     
     
-    #driver.quit()
+    driver.quit()
     
 
-def scraper(driver):
+def scraper(driver, search_term):
     total_word_count = [0,0,0,0,0]
     words_to_count = ['sql', 'python', 'excel', 'tableau']
     
@@ -34,6 +35,35 @@ def scraper(driver):
     for card in base_cards:
         card.click()
         time.sleep(2) # Wait for the job details to load
+        
+        description_text = driver.find_element_by_id('jobDescriptionText').get_attribute('innerHTML')
+        time.sleep(3)
+        
+        soup = BeautifulSoup(description_text, 'lxml')
+        soup_lower = soup.text.lower() #makes all the text lowercase to make it easier to count
+        
+        for i, word in enumerate(words_to_count):
+            word_count = soup_lower.count(word)
+            total_word_count[i] += word_count
+            
+            
+        #Because the programming langauge R is a standalone character r, we have to count it differently.
+        #We use the re package
+        standalone_r = len(re.findall(r'\br\b', soup_lower))
+        total_word_count[4] += standalone_r
+    
+    #Final Output
+    print(f"For {search_term}s:",end = '\n')
+        
+    total_words = ['SQL', 'Python', 'Excel', 'Tableau', 'R']
+        
+    for i, word in enumerate(total_words):
+        print(f"{word} appears {total_word_count[i]} times.")
+            
+    print('\n')
+    
+    time.sleep(2)
+    
 
 if __name__ == "__main__":
     autobot()
